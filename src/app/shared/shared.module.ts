04af3940c4d8d17e49f860534d69b95675e57b84
Aldaine-Legion <vainedev@gmail.com>
2024-05-23 00:29:31 -0500

    Updated (Header): Created header and added tailwind and assets to show in the header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,18 +3,22 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoadingComponent } from './components/loading/loading.component';
 import { ValidationComponent } from './components/validation/validation.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 const modules = [
   ReactiveFormsModule,
 ]
 const declarations = [
-  LoadingComponent
+  LoadingComponent,
+  HeaderComponent,
 ]
 @NgModule({
   declarations: [
     LoadingComponent,
-    ValidationComponent
+    HeaderComponent,
+    ValidationComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
