1b2eb2b5e24c43d3adc9d80a6578ecf667153cf3
Aldaine-Legion <vainedev@gmail.com>
2024-08-06 16:15:53 -0500

    validated the login form

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginform = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    recaptcha: new FormControl('')
+  });
+  showPassword = false;
 
+  togglePasswordVisibility(){
+    this.showPassword = !this.showPassword
+  }
+  login(){
+    console.log(this.loginform.value);
+  }
+  
 }
